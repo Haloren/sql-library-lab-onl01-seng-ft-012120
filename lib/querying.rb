ae02f004932fad90bd1af8cd9c4041cde3c713b1
Kyle McKenna <mckennakyler@gmail.com>
2020-02-19 18:07:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,15 +5,12 @@ def select_books_titles_and_years_in_first_series_order_by_year
   WHERE series.id = 1
   ORDER by books.year;"
 end
-
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM Characters
   ORDER BY motto ASC
   LIMIT 1
   ;"
 end
-
-
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) FROM Characters
 GROUP BY species
@@ -21,11 +18,11 @@ ORDER BY COUNT(species) DESC
 LIMIT 1
 ;"
 end
-
-
-
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM Authors
+JOIN series ON authors.id = series.author_id
+JOIN SubGenres ON SubGenres.id = Series.subgenre_id
+;"
 end
 
 def select_series_title_with_most_human_characters
